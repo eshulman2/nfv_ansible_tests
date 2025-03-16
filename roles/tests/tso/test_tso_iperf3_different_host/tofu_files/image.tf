8732cda9484856ea76a62f17158236e794411297
eshulman2 <ellashulman1@gmail.com>
2025-03-16 16:40:24 +0200

    remove static image parameter

--- roles/tests/tso/test_tso_iperf3_different_host/tofu_files/image.tf
+++ roles/tests/tso/test_tso_iperf3_different_host/tofu_files/image.tf
@@ -1,6 +1,6 @@
 resource "openstack_images_image_v2" "os_image" {
   name             = "best_image"
-  image_source_url = "http://ceph1.lab.eng.tlv2.redhat.com:8080/rdiazcam/rhel-guest-image-8.4-1245-nfv3-shrunk.x86_64.img"
+  image_source_url = local.image
   container_format = "bare"
   disk_format      = "qcow2"
     visibility       = "public"
--- roles/tests/tso/test_tso_iperf3_same_host_geneve/tofu_files/image.tf
+++ roles/tests/tso/test_tso_iperf3_same_host_geneve/tofu_files/image.tf
@@ -1,6 +1,6 @@
 resource "openstack_images_image_v2" "os_image" {
   name             = "best_image"
-  image_source_url = "http://ceph1.lab.eng.tlv2.redhat.com:8080/mnietoji/rhel-guest-image-nfv-2-8.7-1660.x86_64.qcow2"
+  image_source_url = local.image
   container_format = "bare"
   disk_format      = "qcow2"
     visibility       = "public"
